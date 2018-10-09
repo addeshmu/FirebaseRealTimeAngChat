544ed827a285984fa558b0664cefa08933937ea6
Amit Deshmukh <addeshmu@usc.edu>
2018-10-08 20:54:07 -0700

    More mods

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
-
+import { AngularFireDatabase} from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  itemsList: Observable<any[]>;
+  msg: string = '';
+  editMsg:boolean =false;
+  editId: number;
+   constructor(public af: AngularFireDatabase) {
+     this.af = af;
+     this.itemsList = af.list('/messages').snapshotChanges();
+
+   }
+
+   send(chatMsg: string) {
+     this.af.list('/messages').push( {message: chatMsg} );
+     this.msg = '';
+   }
+
+   delete(key:string) {
+     this.af.list('/messages').remove(key);
+   }
+
+   edit(key:string, message:string) {
+     this.af.list('/messages').update(key,{message:message});
+     this.editMsg = false;
+   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyBPKmCKPQPjqtyA_dAGecl-Kt0QI0anHUY",
+    authDomain: "fir-chatapp-77695.firebaseapp.com",
+    databaseURL: "https://fir-chatapp-77695.firebaseio.com",
+    projectId: "fir-chatapp-77695",
+    storageBucket: "fir-chatapp-77695.appspot.com",
+    messagingSenderId: "565642158931"
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
